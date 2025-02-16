09d461c5ddc61e6ed80c558d2291905ed95c2b6c
johnmanjohnston <c.ethanjohn@gmail.com>
2025-02-16 20:05:29 +0400

    UI changes

--- src/editor.cpp
+++ src/editor.cpp
@@ -119,19 +119,42 @@ void AudioPluginAudioProcessorEditor::paint(juce::Graphics &g) {
 
     g.drawFittedText(tmp, getLocalBounds(), juce::Justification::centred, 1);
 
+    // === TOP BAR ===
+    int topBarHeight = 50;
+    // draw top bar bg
+    g.setColour(juce::Colour(0xFFCBCBCB));
+    g.fillRect(0, 0, getWidth(), topBarHeight);
+
+    // draw "track" logo
+    g.setColour(juce::Colour(0xFF727272));
     g.setFont(24.f);
-    g.drawFittedText("track", juce::Rectangle<int>(34, 8, 100, 50),
+    g.drawFittedText("track", juce::Rectangle<int>(34, 1, 100, 50),
                      juce::Justification::left, 1);
 
+    // draw time info bg
+    g.setColour(juce::Colour(0xFF1B1E2D));
+    int timeInfoBgWidth = 350;
+    int timeInfoBgHeight = 40;
+
+    juce::Rectangle<int> timeInfoRectangle =
+        juce::Rectangle<int>((getWidth() / 2) - (timeInfoBgWidth / 2),
+                             (topBarHeight / 2) - (timeInfoBgHeight / 2),
+                             timeInfoBgWidth, timeInfoBgHeight);
+    g.fillRoundedRectangle(timeInfoRectangle.toFloat(), 4.f);
+
+    // draw text for time info
     // bar.beat.division
+    g.setColour(juce::Colour(0xFFCDD8E4));
     juce::String tempoInformationToDisplay = juce::String(bar);
     tempoInformationToDisplay.append(".", 1);
     tempoInformationToDisplay.append(juce::String(beat), 3);
     tempoInformationToDisplay.append(".", 1);
     tempoInformationToDisplay.append(juce::String(division), 3);
 
-    g.drawFittedText(tempoInformationToDisplay,
-                     juce::Rectangle<int>(300, 8, 100, 50),
+    juce::Rectangle<int> timeInfoTextRectangle = timeInfoRectangle;
+    timeInfoTextRectangle.setWidth(timeInfoTextRectangle.getWidth() - 10);
+    timeInfoTextRectangle.setX(timeInfoTextRectangle.getX() + 10);
+    g.drawFittedText(tempoInformationToDisplay, timeInfoTextRectangle,
                      juce::Justification::left, 1);
 }
 
