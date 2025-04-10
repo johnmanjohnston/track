c00f1166a77e3674740ec8d9d45d48bda99a20ac
johnmanjohnston <c.ethanjohn@gmail.com>
2025-04-10 18:44:05 +0400

    add fx logo for plugin chain component; UI changes to plugin chain component

--- src/daw/plugin_chain.cpp
+++ src/daw/plugin_chain.cpp
@@ -7,24 +7,34 @@ track::PluginChainComponent::PluginChainComponent() : juce::Component() {
 track::PluginChainComponent::~PluginChainComponent() {}
 
 void track::PluginChainComponent::paint(juce::Graphics &g) {
-    int margin = 4;
+    int margin = 2;
 
     // bg
-    g.fillAll(juce::Colours::black);
+    g.fillAll(juce::Colour(0xFF'282828));
 
     // titlebar
     juce::Rectangle<int> titlebarBounds = getLocalBounds();
     titlebarBounds.setHeight(titlebarBounds.getHeight() / 9);
+    titlebarBounds.reduce(margin, 0);
     g.setColour(juce::Colours::green);
-    g.fillRect(titlebarBounds);
+    // g.fillRect(titlebarBounds);
 
     g.setColour(juce::Colours::white);
     juce::String x = juce::String(getCorrespondingTrack()->clips.size());
-    g.drawText(getCorrespondingTrack()->trackName + x, titlebarBounds,
-               juce::Justification::left);
+    g.drawText(getCorrespondingTrack()->trackName + x,
+               titlebarBounds.withLeft(50), juce::Justification::left);
+
+    // fx logo
+    float fxLogoDivider = 3.f;
+    juce::Image fxLogo = juce::ImageCache::getFromMemory(
+        BinaryData::FX_png, BinaryData::FX_pngSize);
+    g.drawImageWithin(fxLogo, 0, 0, 90.f / fxLogoDivider, 49.f / fxLogoDivider,
+                      juce::RectanglePlacement::yMid, true);
 
     // border
-    g.setColour(juce::Colours::red);
+    g.setColour(juce::Colour(0xFF'4A4A4A));
+    // g.drawHorizontalLine(titlebarBounds.getHeight(), 0, getWidth());
+    g.fillRect(0, titlebarBounds.getHeight(), getWidth(), 2);
     g.drawRect(getLocalBounds(), 2);
 }
 
